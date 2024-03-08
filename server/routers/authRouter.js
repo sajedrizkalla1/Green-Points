f755671323162ef7b81fbf8ef86e03c187b70073
Mohamad040 <mohamda8@ac.sce.ac.il>
2024-03-08 06:00:03 +0200

    forgetPageAndForgetServer

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -3,6 +3,7 @@ const { authController } = require('../controllers/authController');
 const authRouter = new Router();
 
 authRouter.post('/login', authController.login);
-authRouter.post('/signup', authController.signup); 
+authRouter.post('/signup', authController.signup);
+
 
 module.exports = { authRouter };
