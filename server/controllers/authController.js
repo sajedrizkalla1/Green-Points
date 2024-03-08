8a204e0334be9b91f97d1af967e57277c0e7c44c
Mohamad040 <mohamda8@ac.sce.ac.il>
2024-03-08 06:05:03 +0200

    forgetPageAndForgetServer

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -106,4 +106,77 @@ exports.authController = {
             res.status(500).json({ "message": `Error getting user ` });
         }
     },
+
+    
+    async forgetPassword(req, res) {
+        const { email } = req.body;
+        if (!email) {
+            return res.status(400).json({ "message": "Email is required" });
+        }
+
+        User.findOne({ email: email })
+            .then(user => {
+                if (!user) {
+                    return res.status(404).json({ "message": "User not found" });
+                }
+
+                // Generate a 6-digit OTP
+                const otp = crypto.randomBytes(3).toString('hex').toUpperCase();
+
+                // Store the OTP in the user's document with an expiration time, e.g., 15 minutes
+                user.resetPasswordToken = otp;
+                user.resetPasswordExpires = Date.now() + 15 * 60 * 1000; // 15 minutes
+
+                user.save().then(() => {
+                    // Send the OTP via email
+                    const mailOptions = {
+                        from: 'sajed.r.2001@gmail.com',
+                        to: user.email,
+                        subject: 'Password Reset',
+                        text: `Your OTP for password reset is: ${otp}`
+                    };
+
+                    transporter.sendMail(mailOptions, (error, info) => {
+                        if (error) {
+                            console.log(error);
+                            res.status(500).json({ "message": "Error sending email" });
+                        } else {
+                            res.json({ "message": "An OTP has been sent to your email" });
+                        }
+                    });
+                });
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).json({ "message": "Error finding user" });
+            });
+    },
+
+    async resetPassword(req, res) {
+        const { email, otp, newPassword } = req.body;
+        if (!email || !otp || !newPassword) {
+            return res.status(400).json({ "message": "All fields are required" });
+        }
+
+        User.findOne({ email: email, resetPasswordToken: otp, resetPasswordExpires: { $gt: Date.now() } })
+            .then(user => {
+                if (!user) {
+                    return res.status(400).json({ "message": "Invalid or expired OTP" });
+                }
+
+                // Update the user's password and clear the reset token and expiration
+                user.password = bcrypt.hashSync(newPassword, 8);
+                user.resetPasswordToken = undefined;
+                user.resetPasswordExpires = undefined;
+
+                user.save().then(() => {
+                    res.json({ "message": "Password has been updated " });
+                });
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).json({ "message": "Error resetting password" });
+            });
+    }
+    
 };
--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -2,6 +2,9 @@ const { Router } = require('express');
 const { authController } = require('../controllers/authController');
 const authRouter = new Router();
 
+authRouter.post('/forgetPassword', authController.forgetPassword);
+authRouter.post('/resetPassword', authController.resetPassword);
+
 authRouter.post('/login', authController.login);
 authRouter.post('/signup', authController.signup);
 
